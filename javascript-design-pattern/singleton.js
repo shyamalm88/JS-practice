fbba13153cb7b3ed4ce0c0adf9c66962e0916acc
Shyamalm88 <shyamalm88@gmail.com>
2022-06-09 01:23:08 +0530

    update

--- javascript-design-pattern/singleton.js
+++ javascript-design-pattern/singleton.js
@@ -3,24 +3,24 @@ class Settings {
     if (Settings.instance instanceof Settings) {
       return Settings.instance;
     }
-    this.settingObj = {
+    this.settingsObj = {
       background: "#ff000",
       version: Math.floor(Math.random() * 400),
     };
 
-    Object.freeze(this.settingObj);
+    Object.freeze(this.settingsObj);
     Object.freeze(this);
     Settings.instance = this;
   }
 
   get(key) {
-    return this.settingObj[key];
+    return this.settingsObj[key];
   }
 }
 
 const s = new Settings();
-const b = new Settings();
+const a = new Settings();
 
-console.log(s === b);
+console.log(s === a);
 console.log(s.get("version"));
-console.log(b.get("version"));
+console.log(a.get("version"));
