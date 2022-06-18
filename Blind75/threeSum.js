05bae5391bd6af0ed3e85f90d71700915ae496b4
Shyamalm88 <shyamalm88@gmail.com>
2022-06-19 00:59:11 +0530

    practice update changes

--- Blind75/threeSum.js
+++ Blind75/threeSum.js
@@ -1,24 +1,21 @@
-const threeSum = (numbers, target) => {
-  const nums = numbers.sort((a, b) => {
+const threeSum = function (arr, target) {
+  const nums = arr.sort((a, b) => {
     return a - b;
   });
-
-  let res = [];
-
+  let result = [];
   for (let i = 0; i < nums.length; i++) {
-    if (i > 0 && nums[i] == nums[i - 1]) continue;
-    let targetSum = target - nums[i];
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let targetSum = target - arr[i];
     let left = i + 1;
     let right = nums.length - 1;
-
-    while (right > left) {
+    while (left < right) {
       const sum = nums[left] + nums[right];
       if (sum > targetSum) {
         right--;
       } else if (sum < targetSum) {
         left++;
       } else {
-        res.push([nums[i], nums[left], nums[right]]);
+        result.push([nums[i], nums[left], nums[right]]);
         while (nums[left] === nums[left + 1]) left++;
         while (nums[right] === nums[right - 1]) right--;
         left++;
@@ -26,6 +23,7 @@ const threeSum = (numbers, target) => {
       }
     }
   }
-  return res;
+  return result;
 };
-console.log(threeSum([-1, -2, -3, 4, 5, 6, 7, 0, 1, 2, 3], 0));
+
+console.log(threeSum([-1, -2, 5, 9, 0, 0, 1, 2, 3, 4], 6));
