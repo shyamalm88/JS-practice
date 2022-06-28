6403d758eba562bbeca60cd439297fae8c91eff5
Shyamalm88 <shyamalm88@gmail.com>
2022-06-28 14:04:15 +0530

    updatedIsPalindrome

--- linkedlist/isPalindromLinkedList.js
+++ linkedlist/isPalindromLinkedList.js
@@ -14,10 +14,35 @@ const middleOfLinkedList = (head) => {
     slow = slow.next;
     fast = fast.next.next;
   }
-  console.log(slow.data);
+  // console.log(slow.data);
+  return slow;
 };
 
-const isPalindromeLinkedList = (head) => {};
+const reverseLinkedLIst = (head) => {
+  let prev = null;
+  let current = head;
+  let next = null;
+  while (current != null) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
+
+const isPalindromeLinkedList = (head) => {
+  let fast = head;
+  let slow = reverseLinkedLIst(middleOfLinkedList(head));
+  while (slow != null) {
+    if (slow.data != fast.data) {
+      return false;
+    }
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return true;
+};
 
 const printList = (node) => {
   while (node != null) {
@@ -39,6 +64,8 @@ push(4);
 push(3);
 push(2);
 push(1);
+push(0);
 printList(head);
 console.log("==========");
-middleOfLinkedList(head);
+console.log(isPalindromeLinkedList(head));
+// reverseLinkedLIst(head);
