6d69dfb20ed5007f233096ca9b9961e848ef6652
Shyamalm88 <shyamalm88@gmail.com>
2022-12-03 03:26:41 +0530

    flattenObject

---
+++ Blind75/flatenObject.js
@@ -0,0 +1,28 @@
+let ob = {
+  Company: "GeeksforGeeks",
+  Address: "Noida",
+  contact: +91 - 999999999,
+  mentor: {
+    HTML: "GFG",
+    CSS: "GFG",
+    JavaScript: "GFG",
+  },
+};
+
+const flattenObj = (ob) => {
+  let result = {};
+
+  for (const i in ob) {
+    if (typeof ob[i] === "object" && !Array.isArray(ob[i])) {
+      const temp = flattenObj(ob[i]);
+      for (const j in temp) {
+        result[i + "_" + j] = temp[j];
+      }
+    } else {
+      result[i] = ob[i];
+    }
+  }
+  return result;
+};
+
+console.log(flattenObj(ob));
