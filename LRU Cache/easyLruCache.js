cd2e3c06012330e69f54b31e32714cd68d281171
Shyamalm88 <shyamalm88@gmail.com>
2022-12-29 03:14:44 +0530

    updated

--- LRU Cache/easyLruCache.js
+++ LRU Cache/easyLruCache.js
@@ -5,14 +5,13 @@ class LRU {
   }
 
   get(key) {
-    const item = this.cache.has(key);
+    const item = this.cache.get(key);
     if (item) {
       this.cache.delete(key);
-      this.cache.set(key, value);
+      this.cache.set(key, item);
     }
     return item;
   }
-
   set(key, value) {
     if (this.cache.get(key)) {
       this.cache.delete(key);
@@ -23,7 +22,6 @@ class LRU {
   }
 
   first() {
-    console.log(this.cache.keys().next());
     return this.cache.keys().next().value;
   }
 }
@@ -34,5 +32,6 @@ cache.set("name", "Arghya");
 cache.set("age", 34);
 cache.set("language", "bengali");
 console.log(cache.cache);
+cache.get("name");
 cache.set("occupation", "developer");
 console.log(cache.cache);
