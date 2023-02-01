b066ac33e4a082c4fd8d00644245882b76513456
Shyamalm88 <shyamalm88@gmail.com>
2023-02-01 15:13:56 +0530

    updated

---
+++ Blind75/AddTwoNumbersLinkedList.js
@@ -0,0 +1,23 @@
+// You are given two non-empty linked lists representing two non-negative integers.
+// The digits are stored in reverse order and each of their nodes contain a single digit.
+// Add the two numbers and return it as a linked list.
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+var addTwoNumbers = function (l1, l2) {
+  var carry = 0;
+  var sum = 0;
+  var head = new ListNode(0);
+  var now = head;
+  var a = l1;
+  var b = l2;
+  while (a !== null || b !== null) {
+    sum = (a ? a.val : 0) + (b ? b.val : 0) + carry;
+    carry = Math.floor(sum / 10);
+    now.next = new ListNode(sum % 10);
+    now = now.next;
+    a = a ? a.next : null;
+    b = b ? b.next : null;
+  }
+  if (carry) now.next = new ListNode(carry);
+  return head.next;
+};
---
+++ Blind75/InterSectionOfTwoLinkedList.js
@@ -0,0 +1,11 @@
+var getIntersectionNode = function (headA, headB) {
+  let a = headA;
+  let b = headB;
+
+  while (a != b) {
+    a = !a ? headB : a.next;
+    b = !b ? headA : b.next;
+  }
+
+  return a;
+};
