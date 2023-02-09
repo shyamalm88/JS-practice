452842a435577fc61687bae30375fcf26868d4cb
Shyamalm88 <shyamalm88@gmail.com>
2023-02-09 20:56:47 +0530

    updated flattern array

--- Array/flaternArrayPrototype.js
+++ Array/flaternArrayPrototype.js
@@ -10,4 +10,19 @@ Array.prototype.flattern = function () {
   return res;
 };
 
-console.log([1, 2, [3, 4, 5, [6, 7, [8, 9, 10, [89, 56]]]]].flattern());
+Array.prototype.flatternDepth = function (d) {
+  let res = [];
+  for (let i = 0; i < this.length; i++) {
+    if (d && Array.isArray(this[i])) {
+      d--;
+      res = res.concat(this[i].flatternDepth(d));
+    } else {
+      res.push(this[i]);
+    }
+  }
+  return res;
+};
+
+console.log([1, 2, [3, 4, 5, [6, 7, [8, 9, 10, [89, 56]]]]].flatternDepth(2));
+
+// console.log([1, 2, [3, 4, 5, [6, 7, [8, 9, 10, [89, 56]]]]].flattern());
