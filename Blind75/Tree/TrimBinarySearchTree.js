4aa76de6d061fc838de27d505aba247ce778d62b
Shyamalm88 <shyamalm88@gmail.com>
2023-02-12 13:00:13 +0530

    trim binary search tree

--- Blind75/Tree/TrimBinarySearchTree.js
+++ Blind75/Tree/TrimBinarySearchTree.js
@@ -1,5 +1,8 @@
 /**
- * Given a binary search tree and the lowest and highest boundaries as L and R, trim the tree so that all its elements lies in [L, R] (R >= L). You might need to change the root of the tree, so the result should return the new root of the trimmed binary search tree.
+ * Given a binary search tree and the lowest and highest boundaries as L and R, 
+ * trim the tree so that all its elements lies in [L, R] (R >= L). 
+ * You might need to change the root of the tree, so the result should return 
+ * the new root of the trimmed binary search tree.
 
 Example 1:
 
@@ -36,3 +39,12 @@ Output:
   /
  1
  */
+
+var trimBST = function (root, L, R) {
+  if (!root) return null;
+  if (root.val < L) return trimBST(root.right, L, R);
+  if (root.val > R) return trimBST(root.left, L, R);
+  root.left = trimBST(root.left, L, R);
+  root.right = trimBST(root.right, L, R);
+  return root;
+};
