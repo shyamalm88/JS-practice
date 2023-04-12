13cab7e832e029b04685d78339ec38a48f8badf0
Shyamalm88 <shyamalm88@gmail.com>
2023-04-13 00:30:04 +0530

    promise.allSettled without Promise.all

--- polyfils/promise.allSettled.polyfil.js
+++ polyfils/promise.allSettled.polyfil.js
@@ -17,6 +17,30 @@ Promise.myAllSettled = function (promises) {
   return Promise.all(mappedPromises);
 };
 
+// without Promise.all
+Promise.myAllSettled2 = (promises) => {
+  const results = new Array(promises.length);
+  let counter = 0;
+
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((res) => {
+          results[index] = { status: "fulfilled", value: res };
+        })
+        .catch((err) => {
+          results[index] = { status: "rejected", reason: err };
+        })
+        .finally(() => {
+          counter++;
+          if (counter === promises.length) {
+            resolve(results);
+          }
+        });
+    });
+  });
+};
+
 const promise1 = new Promise((resolve, reject) => {
   setTimeout(resolve, 500, "one");
 });
